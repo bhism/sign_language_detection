0ecf9a236025ef3748b56ce5f9d6ae86934e6aa0
BHISM <61289719+bhism@users.noreply.github.com>
2021-11-15 10:53:50 +0530

    Add files via upload

---
+++ home.dart
@@ -0,0 +1,157 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:tflite/tflite.dart';
+
+import 'main.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String answer = '';
+  CameraController? cameraController;
+  CameraImage? cameraImage;
+
+  loadmodel() async {
+    Tflite.loadModel(
+      model: 'assets/model_unquant.tflite',
+      labels: 'assets/labels.txt',
+    );
+  }
+
+  initCamera() {
+    cameraController = CameraController(cameras![0], ResolutionPreset.medium);
+    cameraController!.initialize().then(
+      (value) {
+        if (!mounted) {
+          return;
+        }
+        setState(
+          () {
+            cameraController!.startImageStream(
+              (image) => {
+                if (true)
+                  {
+                    cameraImage = image,
+                    applymodelonimages(),
+                  }
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+
+  applymodelonimages() async {
+    if (cameraImage != null) {
+      var predictions = await Tflite.runModelOnFrame(
+          bytesList: cameraImage!.planes.map(
+            (plane) {
+              return plane.bytes;
+            },
+          ).toList(),
+          imageHeight: cameraImage!.height,
+          imageWidth: cameraImage!.width,
+          imageMean: 127.5,
+          imageStd: 127.5,
+          rotation: 90,
+          numResults: 3,
+          threshold: 0.1,
+          asynch: true);
+
+      answer = '';
+
+      predictions!.forEach(
+        (prediction) {
+          answer +=
+              prediction['label'].toString().substring(0, 1).toUpperCase() +
+                  prediction['label'].toString().substring(1) +
+                  " " +
+                  (prediction['confidence'] as double).toStringAsFixed(3) +
+                  '\n';
+        },
+      );
+
+      setState(
+        () {
+          answer = answer;
+        },
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initCamera();
+    loadmodel();
+  }
+
+  @override
+  void dispose() async {
+    super.dispose();
+
+    await Tflite.close();
+    cameraController!.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme:
+          ThemeData(brightness: Brightness.dark, primaryColor: Colors.purple),
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: cameraImage != null
+              ? Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.blue,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        child: Center(
+                          child: Container(
+                            height: MediaQuery.of(context).size.height,
+                            width: MediaQuery.of(context).size.width,
+                            child: AspectRatio(
+                              aspectRatio: cameraController!.value.aspectRatio,
+                              child: CameraPreview(
+                                cameraController!,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Center(
+                          child: Container(
+                            padding: EdgeInsets.all(10),
+                            color: Colors.black87,
+                            child: Center(
+                              child: Text(
+                                answer,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              : Container(),
+        ),
+      ),
+    );
+  }
+}
---
+++ main.dart
@@ -0,0 +1,66 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+List<CameraDescription>? cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.indigo[900],
+      ),
+      home: FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatefulWidget {
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    var h = MediaQuery.of(context).size.height;
+    var w = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.indigo[900],
+      body: Center(
+        child: Container(
+          color: Theme.of(context).primaryColor,
+          child: Container(
+            margin: EdgeInsets.all(20),
+            height: 50,
+            width: w,
+            child: MaterialButton(
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Home(),
+                  ),
+                );
+              },
+              child: Text(
+                'Start Detecting',
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
